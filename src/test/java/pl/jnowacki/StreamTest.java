7db05d7e2333d76a7d2c17324a2326cde3df4f0e
Jedrzej Nowacki <contact@jnowacki.pl>
2019-10-27 10:55:18 +0100

    Stream test

---
+++ src/test/java/pl/jnowacki/StreamTest.java
@@ -0,0 +1,17 @@
+package pl.jnowacki;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTest {
+
+    private List<Integer> numbers = new ArrayList<>();
+
+    @Test
+    public void testStreamNumbers(){
+
+    }
+}
